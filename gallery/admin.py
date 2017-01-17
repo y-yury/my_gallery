a70caf851e5c278f9dce75851b5e4f644a8ae067
Yury Sapazhkou <yury.sapozhkov@kapsch.by>
2017-01-17 13:39:47 +0300

    Class 'Photo' was initialised in models.py with key fields & image properties,  1st migration was performed.

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Photo
 
-# Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp',)
+    list_display_links = ('title',)
+    search_fields = ('title',)
+
+admin.site.register(Photo, PhotoAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Photo(models.Model):
+    title = models.CharField(max_length=250)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    image = models.ImageField(null=False, blank=False, width_field="width", height_field="height")
+    timestamp = models.DateTimeField(default=timezone.now, auto_now=False)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-timestamp"]
