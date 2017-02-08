2c2ce7bc62c292acfbeaf0895087be032e89d8f4
Yury Sapazhkou <yury.sapozhkov@kapsch.by>
2017-02-08 22:44:58 +0300

    Minor changes in template & admin back-end performed.

--- gallery/admin.py
+++ gallery/admin.py
@@ -15,7 +15,7 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 class EmailAdmin(admin.ModelAdmin):
-    list_display = ('name', 'subject', 'e_mail', 'message',)
+    list_display = ('name', 'subject', 'e_mail', 'message', 'date_submitted',)
     list_display_links = ('name',)
     search_fields = ('name', 'e_mail',)
 
